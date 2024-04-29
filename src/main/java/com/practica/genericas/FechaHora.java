a17506515d2e679e9e810fe935d4b2d405b0d665
Rodrigo Alonzo <rodrigo.pvigil@alumnos.upm.es>
2024-04-29 20:46:43 +0200

    Se hicieron los ultimos 8 cambios (low)

--- src/main/java/com/practica/genericas/FechaHora.java
+++ src/main/java/com/practica/genericas/FechaHora.java
@@ -1,7 +1,7 @@
 package com.practica.genericas;
 
 
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 
 public class FechaHora implements Comparable<FechaHora>{
@@ -114,10 +114,10 @@ public class FechaHora implements Comparable<FechaHora>{
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+		final int PRIME = 31;
 		int result = 1;
-		result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
-		result = prime * result + ((hora == null) ? 0 : hora.hashCode());
+		result = PRIME * result + ((fecha == null) ? 0 : fecha.hashCode());
+		result = PRIME * result + ((hora == null) ? 0 : hora.hashCode());
 		return result;
 	}
 
